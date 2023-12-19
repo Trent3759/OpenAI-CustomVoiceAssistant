8255b62dc5b840630bfa4be7a6b35e3676c361de
Trenton Scott <trentondanscott@gmail.com>
2023-12-18 18:36:36 -0600

    Updating structure for class functionality

--- app.py
+++ app.py
@@ -5,92 +5,83 @@ from threading import Timer
 from openai import OpenAI
 import config as cfg
 
-# Initialize OpenAI client
-client = OpenAI(api_key=cfg.OPENAI_API_KEY)
+class ChatBot:
+    def __init__(self, voice="alloy"):
+        self.client = OpenAI(api_key=cfg.OPENAI_API_KEY)
+        self.r = sr.Recognizer()
+        self.listening = False
+        self.t = Timer(30.0, self.set_listening_true)
+        self.messages = [
+            {"role": "system", "content": "I am " + cfg.BOT_NAME.capitalize() + "."},
+            {"role": "system", "content": "I give short responses that will be spoken. All responses will be conversational."},
+            {"role": "system", "content": cfg.CUSTOM_COMMAND}
+        ]
 
-# Speech recognition setup
-r = sr.Recognizer()
+    def set_listening_true(self):
+        self.listening = False
 
-# Main program loop
-listening = False
+    def listen_for_input(self):
+        with sr.Microphone() as source:
+            audio = self.r.listen(source)
+            try:
+                text = self.r.recognize_google(audio)
+                return text
+            except sr.RequestError:
+                print("Sorry, there was an error with the speech recognition service.")
+                return ""
+            except:
+                return ""
 
-# Function to set listening to True
-def set_listening_true():
-    global listening
-    listening = False
+    def get_chatbot_response(self, messages):
+        response = self.client.chat.completions.create(
+            model="gpt-4",
+            messages=messages,
+            max_tokens=500,
+            frequency_penalty=0.4,
+            presence_penalty=0.5,
+            stop=["Outline:"]
+        )
+        messages.append({"role": "system", "content": response.choices[0].message.content})
+        safe_response = response.choices[0].message.content
+        self.generate_audio(safe_response, cfg.VOICE)
+        return safe_response
 
-# Timer setup
-t = Timer(30.0, set_listening_true)
+    def play_audio(self, file_path):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        pygame.mixer.quit()
+        pygame.quit()
 
-# Function to convert speech to text
-def listen_for_input():
-    with sr.Microphone() as source:
-        audio = r.listen(source)
+    def generate_audio(self, paragraph, voice="alloy"):
+        response = self.client.audio.speech.create(
+            model="tts-1",
+            voice=voice,
+            input=paragraph,
+        )
+        root = os.path.dirname(os.path.realpath(__file__))
+        response.stream_to_file(root + "\\output.mp3")
+        self.play_audio(root + "\\output.mp3")
         try:
-            text = r.recognize_google(audio)
-            return text
-        except sr.RequestError:
-            print("Sorry, there was an error with the speech recognition service.")
-            return ""
+            self.t.cancel()
         except:
-            return ""
+            pass
+        self.t = Timer(30.0, self.set_listening_true)
+        self.t.start()
 
-# Function to send user input to ChatGPT and get a response
-def get_chatbot_response(messages):
-    response = client.chat.completions.create(
-        model="gpt-4",
-        messages=messages,
-        max_tokens=500,
-        frequency_penalty=0.4,
-        presence_penalty=0.5,
-        stop=["Outline:"]
-    )
-    messages.append({"role": "system", "content": response.choices[0].message.content})
-    safe_response = response.choices[0].message.content
-    generate_audio(safe_response, cfg.VOICE)
-    return safe_response
+    def run(self):
+        while True:
+            user_input = self.listen_for_input()
+            if user_input and ((cfg.BOT_NAME.lower() in user_input or cfg.BOT_NAME.capitalize() in user_input) or self.listening):
+                print("You: " + user_input)
+                self.listening = True
+                self.messages.append({"role": "user", "content": user_input})
+                response = self.get_chatbot_response(self.messages)
+                print(cfg.BOT_NAME.capitalize() + ": " + response)
 
-# Function to play audio
-def play_audio(file_path):
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-    pygame.mixer.quit()
-    pygame.quit()
-
-# Function to generate audio from text
-def generate_audio(paragraph, voice="alloy"):
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice=voice,
-        input=paragraph,
-    )
-    root = os.path.dirname(os.path.realpath(__file__))
-    response.stream_to_file(root + "\\output.mp3")
-    play_audio(root + "\\output.mp3")
-    global t
-    try:
-        t.cancel()
-    except:
-        pass
-    #reset timer
-    t = Timer(30.0, set_listening_true)
-    t.start()
-
-messages = [
-    {"role": "system", "content": "I am " + cfg.BOT_NAME.capitalize() + "."},
-    {"role": "system", "content": "I give short responses that will be spoken. All responses will be conversational."},
-    {"role": "system", "content": cfg.CUSTOM_COMMAND}
-]
-
-while True:
-    user_input = listen_for_input()
-    if user_input and ((cfg.BOT_NAME.lower() in user_input or cfg.BOT_NAME.capitalize() in user_input) or listening):
-        print("You: " + user_input)
-        listening = True
-        messages.append({"role": "user", "content": user_input})
-        response = get_chatbot_response(messages)
-        print(cfg.BOT_NAME.capitalize() + ": " + response)
+if __name__ == "__main__":
+    bot = ChatBot()
+    bot.run()
